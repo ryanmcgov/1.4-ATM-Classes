31836a866bb657774b5e1110eb1018f3c9eab96c
eastcoastsb <ryanmcgovern@Ryans-MacBook-Air.local>
2015-06-22 08:32:05 -0400

    ATM options

--- assignment.rb
+++ assignment.rb
@@ -33,7 +33,7 @@ csv = CSV.foreach('./users.csv', :headers => true, :header_converters => :symbol
   accounts.push(user)
 }
 
-def user_input(text)
+def prompt_input(text)
   input = ""
   while input == ""
     puts text
@@ -43,3 +43,50 @@ def user_input(text)
   input
 end
 
+current_user_name = ""
+while current_user_name == ""
+  current_user_name = prompt_user("Please Enter Your Name")
+end 
+
+current_user_pin = ""
+while current_user_pin == ""
+  current_user_pin = prompt_user("Please Enter Your Pin")
+end
+
+user_exists = false
+current_user = nil
+
+existing_users.each do |user|
+  if user[ :name] == current_user_name && user[ :pin] == current_user_pin
+    current_user = user 
+  end
+end
+
+if current_user
+  options = ["1. Check Balance", "2. Withdraw Funds", "3. Cancel"]
+  selected_option = nil
+  until (1..options.length).include?(selected_option) do
+    selected_option = prompt_user(options.join("\n")).to_i
+  end
+  
+  case selected_option
+  when 1
+    puts "Your Balance Is #{current_user[:available_funds]}"
+  when 2
+    requested_amount = prompt_user("How Much Do You Want?")
+    if requested_amount >= atm_available_funds
+      puts "This machine does not have enough funds"
+    elsif requested_amount >= current_user[:available_funds]
+      puts "Insufficient funds"
+    else
+      puts "Please Take Your Money"
+      current_user[:available_funds] -= requested_amount
+      puts "New Balance: #{current_user[:available_funds]}"
+    end
+    
+  when 3
+    puts "Thank You, Come Again!"
+  end
+else
+  puts "Incorrect Information"
+end
